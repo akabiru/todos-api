274b74f2f7a49428982e5d39f5702c1bcb3eba5f
Austin Kabiru <austin.kabiru@andela.com>
2017-03-07 15:15:17 +0300

    implement auth controller

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,14 @@
+class AuthenticationController < ApplicationController
+  # return auth token once user is authenticated
+  def authenticate
+    auth_token =
+      AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
+    json_response(auth_token: auth_token)
+  end
+
+  private
+
+  def auth_params
+    params.permit(:email, :password)
+  end
+end
